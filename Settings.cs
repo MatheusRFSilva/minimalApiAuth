2cf0c2d0b73587a6336ea77dce715fce36d9dce6
Matheus Silva <matheusuniversal@yahoo.com.br>
2021-11-01 18:18:28 -0300

    Commit initial

---
+++ Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinimalApiAuth
+{
+    public class Settings
+    {
+        public static string Secret { get; set; } = "cc8d7ef15f9644afbb3a381b61e39456";
+    }
+}
